c3dcd5ddb39876eec5635f5daabba481abae1ad0
Michael R.F <mrflowers22@uwalumni.com>
2020-01-28 20:52:28 -0800

    implemented functionality for the tableView

--- EntryListTableViewController.swift
+++ EntryListTableViewController.swift
@@ -30,8 +30,8 @@ class EntryListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
         let entry  = EntryController.shared.fetchedResultsController.object(at: indexPath)
-        cell.textLabel.text = entry.name
-        
+        cell.textLabel?.text = entry.name
+        cell.detailTextLabel?.text = entry.timestamp?.prettyDate()
         // Configure the cell...
 
         return cell
@@ -40,19 +40,26 @@ class EntryListTableViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+             let entry = EntryController.shared.fetchedResultsController.object(at: indexPath)
+            EntryController.shared.delete(entry: entry)
+            
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "cellSegue" {
+            guard let destinationVC = segue.destination as? DetailEntryViewController, let indexPath =  tableView.indexPathForSelectedRow else {
+                print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                return
+            }
+            let entry = EntryController.shared.fetchedResultsController.object(at: indexPath)
+            destinationVC.entry = entry
+        }
     }
 }
 
