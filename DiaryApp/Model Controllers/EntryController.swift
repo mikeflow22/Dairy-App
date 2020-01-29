01a71e9ffb5fcc552aa2193e070821aa2e86b839
Michael R.F <mrflowers22@uwalumni.com>
2020-01-28 20:24:45 -0800

    implemented the EntryController class

---
+++ DiaryApp/Model Controllers/EntryController.swift
@@ -0,0 +1,80 @@
+//
+//  EntryController.swift
+//  DiaryApp
+//
+//  Created by Michael Flowers on 1/28/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class EntryController {
+    static let shared = EntryController()
+    
+    var fetchedResultsController: NSFetchedResultsController<Entry>
+    
+    init(){
+        let fetchRequst: NSFetchRequest<Entry> =  Entry.fetchRequest()
+        fetchRequst.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: true)]
+        let resultsController = NSFetchedResultsController(fetchRequest: fetchRequst, managedObjectContext: CoreDataStack.shared.mainContext, sectionNameKeyPath: nil, cacheName: nil)
+        self.fetchedResultsController = resultsController
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch  {
+            print("Error in: \(#function)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)")
+        }
+    }
+    
+//    var entries: [Entry] {
+//        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+//        return (try? CoreDataStack.shared.mainContext.fetch(fetchRequest)) ?? []
+//    }
+    
+    
+    func createEntryWith(name: String, body: String, longitude: Double, latitude: Double){
+        Entry(name: name, body: body, longitude: longitude, latitude: latitude)
+        saveToPersistentStore()
+    }
+    
+    func delete(entry: Entry){
+        entry.managedObjectContext?.delete(entry)
+        saveToPersistentStore()
+    }
+    
+    func update(entry: Entry, withNewName name: String, withNewBody body: String, withNewLongitude longitude: Double, withNewLatitude latitude: Double, withNewTimestamp timestamp: Date = Date()){
+        entry.name = name
+        entry.body = body
+        entry.longitude = longitude
+        entry.latitude = latitude
+        entry.timestamp = timestamp
+        saveToPersistentStore()
+    }
+    
+    func saveToPersistentStore(){
+        do {
+            try CoreDataStack.shared.mainContext.save()
+        } catch  {
+            print("Error in: \(#function)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)")
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
